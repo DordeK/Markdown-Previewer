991c0aae59183efbac2413a197d44028313e4bc7
DordeK <@aleks.dorde@gmail.com>
2020-08-25 02:12:47 +0200

    Updates

---
+++ precache-manifest.3a8210ef61e2404e3bf1cb364b409261.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9560b6f6dc48ca18432b97d29bdf5c02",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "5a75a5fb7152aa9ff280",
+    "url": "/Markdown-Previewer/static/css/main.f16e174a.chunk.css"
+  },
+  {
+    "revision": "3677a1d36608d2ac8082",
+    "url": "/Markdown-Previewer/static/js/2.0c9a7a87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Markdown-Previewer/static/js/2.0c9a7a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a75a5fb7152aa9ff280",
+    "url": "/Markdown-Previewer/static/js/main.b8bf45cf.chunk.js"
+  },
+  {
+    "revision": "c86c511cbf983f30b75e",
+    "url": "/Markdown-Previewer/static/js/runtime-main.1593bf09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Markdown-Previewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
